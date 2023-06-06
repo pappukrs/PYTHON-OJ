78945eb614a54db35cd40e196296d87f055aebc5
pappukrs <digitalpappu101@gmail.com>
2023-06-06 15:52:14 +0530

    switch if elif else statement

---
+++ BASIC/demo.py
@@ -0,0 +1,55 @@
+
+# def conditional(num):
+#     if num==1:
+#         print("EQUAL NUMBER");
+#     elif num>1:
+#         print("Number greater than 1");
+#     else:
+#         print("Number is must less than 1");
+
+# conditional(-2);
+
+# def Sum(a,b):
+#     x=a+b;
+#     print("The sum of number is ",x);
+# Sum(1,2);
+
+num=input("Write the name of?");
+match num:
+      case 1:
+       print("Monday");
+      case 2:
+       print("Tuesday");
+      case 3:
+       print("Wednesday");
+      case 4:
+       print("Thursday");
+      case 5:
+       print("Friday");
+      case 6:
+       print("Saturday");
+      case 7:
+       print("Sunday");
+
+
+
+# def Week(num):
+#     if num==1:
+#         print("Monday");
+#     elif num==2:
+#         print("Tuesday");
+#     elif num==3:
+#         print("Wednesday");
+#     elif num==4:
+#         print("Thursday");
+#     elif num==5:
+#         print("Friday");
+#     elif num==6:
+#         print("Saturday");
+#     elif num==7:
+#         print("Sunday");
+# Week(7);
+
+
+
+    
