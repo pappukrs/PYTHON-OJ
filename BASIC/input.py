e7b9b90a087ea37b2bd64a39339784163fc53348
pappukrs <digitalpappu101@gmail.com>
2023-06-06 16:22:38 +0530

    single line input

---
+++ BASIC/input.py
@@ -0,0 +1,29 @@
+# name=input("Enter your name: ");
+# print(name,type(name));
+arr=[] ;
+
+arr.append(1);
+arr.append(6);
+arr.append(89);
+arr.append(67);
+arr.append(78);
+print(arr,"arr");
+for t in arr:
+    if t%2==0:
+        print("Even Number:",t);
+    else:
+        print("Odd Number: ",t);
+
+x=input("Enter the number:").split();
+# print(int(x)+1);
+# print("y",int(y)+1);
+
+for t in x:
+    print(int(t)+1);
+
+
+
+
+
+
+
